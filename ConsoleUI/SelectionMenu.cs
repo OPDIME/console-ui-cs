5ae8c7a4603eceade745f4c316f03969b319c93f
Jörn Neumeyer <joernneumeyer@gmail.com>
2018-07-27 17:42:42 +0200

    adjusted root namespace

--- ConsoleUI/SelectionMenu.cs
+++ ConsoleUI/SelectionMenu.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.ExceptionServices;
+using ConsoleUI;
 
-namespace ConsoleUI
+namespace OPDIME.ConsoleUI
 {
   public class SelectionMenu
   {
